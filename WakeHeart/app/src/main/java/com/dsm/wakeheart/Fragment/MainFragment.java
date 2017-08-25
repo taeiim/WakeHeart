8a1c9b568a0a7c86bb2b1bbef8dbe6c4737299cf
taeiim <ti535666@gmail.com>
2017-08-25 13:39:37 +0900

    MainPage xml

--- WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/MainFragment.java
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/MainFragment.java
@@ -6,8 +6,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.dsm.wakeheart.R;
+import com.txusballesteros.SnakeView;
 
 /**
  * Created by parktaeim on 2017. 8. 25..
@@ -15,10 +18,43 @@ import com.dsm.wakeheart.R;
 
 public class MainFragment extends android.support.v4.app.Fragment {
 
+    Button onOff_Btn;
+    RelativeLayout offLayout;
+    RelativeLayout onLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+        final View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+
+
+        onOff_Btn = (Button) rootView.findViewById(R.id.OnOffButton);
+        onOff_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                offLayout = (RelativeLayout) rootView.findViewById(R.id.offLayout);
+                offLayout.setVisibility(View.GONE);
+                onLayout = (RelativeLayout) rootView.findViewById(R.id.onLayout);
+                onLayout.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        onLayout = (RelativeLayout) rootView.findViewById(R.id.onLayout);
+        onLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                offLayout = (RelativeLayout) rootView.findViewById(R.id.offLayout);
+                offLayout.setVisibility(View.VISIBLE);
+
+                onLayout.setVisibility(View.GONE);
+            }
+        });
+
+        final SnakeView snakeView = (SnakeView) rootView.findViewById(R.id.snake);
+        snakeView.setMinValue(0);
+        snakeView.setMaxValue(150);
+
         return rootView;
     }
 }
