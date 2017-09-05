2d8f7c4be321ed675abf9ac01d628524ae1aa82b
taeiim <ti535666@gmail.com>
2017-09-05 17:19:31 +0900

    Create Tab in HelperFragment

--- WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/HelperFragment.java
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/HelperFragment.java
@@ -4,28 +4,98 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TabHost;
+import com.nhn.android.maps.NMapView;
 
 import com.dsm.wakeheart.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by parktaeim on 2017. 8. 25..
  */
 
 public class HelperFragment extends android.support.v4.app.Fragment {
+//    private NMapView mMapView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_helper,container,false);
 
-        
+        ViewPager viewPager = (ViewPager) rootView.findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+
+        TabLayout tabs = (TabLayout) rootView.findViewById(R.id.result_tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
+
+
+//        mMapView = (NMapView)rootView.findViewById(R.id.mapView);
+//
+//        mMapView = new NMapView(getActivity().getApplicationContext());
+//        mMapView.setClientId("oPDj1y9Xk6B_IakJkQ0J");
+//
+//        // initialize map view
+//        mMapView.setClickable(true);
+//        mMapView.setEnabled(true);
+//        mMapView.setFocusable(true);
+//        mMapView.setFocusableInTouchMode(true);
+//        mMapView.requestFocus();
 
         return rootView;
 
 
     }
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new RestAreaFragment(), "휴게소");
+        adapter.addFragment(new WiseSayingFragment(), "명언");
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<android.support.v4.app.Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(android.support.v4.app.Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
---
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/RestAreaFragment.java
@@ -0,0 +1,10 @@
+package com.dsm.wakeheart.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by parktaeim on 2017. 9. 5..
+ */
+
+public class RestAreaFragment extends Fragment {
+}
