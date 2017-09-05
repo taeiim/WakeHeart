7693093560916426e458f5c53fee801953816be2
Smaker <admin@smaker.kr>
2017-09-05 19:09:32 +0900

    Add StatusController.java

--- WakeHeart-Server/src/kr/smaker/wakeheart/controller/HomeController.java
+++ WakeHeart-Server/src/kr/smaker/wakeheart/controller/HomeController.java
@@ -4,11 +4,13 @@ import org.servoframework.annotation.Route;
 import org.servoframework.request.Request;
 import org.servoframework.response.Response;
 
+
 public class HomeController {
 	@Route(route = "/", method = Route.RouteMethod.GET)
     public static void home(Request req, Response res) {
         res.setHeader("Content-Type", "text/html");
         res.write("Hello World");
         res.end();
-    }
+	}
+	
 }
