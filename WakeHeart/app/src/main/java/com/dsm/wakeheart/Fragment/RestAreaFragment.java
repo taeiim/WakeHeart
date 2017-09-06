cbd9bba9efb46074bac907cdfa7712937226ae12
taeiim <ti535666@gmail.com>
2017-09-06 11:01:02 +0900

    Add Naver Map API in RestAreaFragment

--- WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/RestAreaFragment.java
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/RestAreaFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.nhn.android.maps.NMapContext;
+import com.nhn.android.maps.NMapView;
 
 import com.dsm.wakeheart.R;
 
@@ -14,10 +16,63 @@ import com.dsm.wakeheart.R;
  */
 
 public class RestAreaFragment extends Fragment {
+    private NMapContext nMapContext;
+    private static final String CLIENT_ID = "oPDj1y9Xk6B_IakJkQ0J";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_helper_restarea,container,false);
+
         return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        nMapContext = new NMapContext(super.getActivity());
+        nMapContext.onCreate();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        NMapView mapView = (NMapView) getView().findViewById(R.id.mapView);
+        mapView.setClientId(CLIENT_ID);
+        nMapContext.setupMapView(mapView);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        nMapContext.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        nMapContext.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        nMapContext.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        nMapContext.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        nMapContext.onDestroy();
+        super.onDestroy();
     }
 }
