ac664e0f95f55927437c2974a1f436bcd6ee0cd8
taeiim <ti535666@gmail.com>
2017-09-26 23:21:16 +0900

    modified music service

--- WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/MainFragment.java
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Fragment/MainFragment.java
@@ -75,8 +75,16 @@ public class MainFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        sleep();
 
         return rootView;
+    }
+
+    private void sleep() {
+        sleep = true;
+        if(sleep == true){
+            getActivity().startService(new Intent(getActivity(),AlarmService.class));
+        }
     }
 
 
