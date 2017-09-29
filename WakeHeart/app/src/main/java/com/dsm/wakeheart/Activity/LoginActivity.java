831d433769bb898249f9b88cc388c246f3211bee
taeiim <ti535666@gmail.com>
2017-09-29 22:39:47 +0900

    Login retrofit, modified bottomNavigation error

--- WakeHeart/app/src/main/java/com/dsm/wakeheart/Activity/LoginActivity.java
+++ WakeHeart/app/src/main/java/com/dsm/wakeheart/Activity/LoginActivity.java
@@ -3,18 +3,19 @@ package com.dsm.wakeheart.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatEditText;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.dsm.wakeheart.Model.LoginItem;
 import com.dsm.wakeheart.R;
 import com.dsm.wakeheart.RestAPI;
 import com.dsm.wakeheart.Server.resource.APIUrl;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -29,8 +30,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LoginActivity extends AppCompatActivity {
     Context context;
-    private AppCompatEditText input_id;
-    private AppCompatEditText input_pw;
+    private TextInputEditText input_id;
+    private TextInputEditText input_pw;
     private Button loginBtn;
     String id;
     String pw;
@@ -60,16 +61,16 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void login() {
-        input_id = (AppCompatEditText) findViewById(R.id.input_id);
-        input_pw = (AppCompatEditText) findViewById(R.id.input_pw);
-
-        id = input_id.getText().toString();
-        pw = input_pw.getText().toString();
+        input_id = (TextInputEditText) findViewById(R.id.input_id);
+        input_pw = (TextInputEditText) findViewById(R.id.input_pw);
 
         loginBtn = (Button) findViewById(R.id.LoginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                id = input_id.getText().toString();
+                pw = input_pw.getText().toString();
 
                 //id,pw가 입력되지 않았으면
                 if(input_id==null || input_id.length() ==0 ){
@@ -85,20 +86,25 @@ public class LoginActivity extends AppCompatActivity {
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
                 RestAPI restAPI = builder.create(RestAPI.class);
-                Call<ResponseBody> call = restAPI.logIn(new LoginItem(id,pw));
+                Call<JsonObject> call = restAPI.logIn(id,pw);
 
-                call.enqueue(new Callback<ResponseBody>() {
+                call.enqueue(new Callback<JsonObject>() {
                     @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                         String stringResponse = response.body().toString();
-                        Log.i("response--------- ",response.body().toString());
-                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                        startActivity(intent);
-                        finish();
+                        Log.i("response----------",stringResponse);
+
+                        JsonElement jsonElement = response.body().getAsJsonPrimitive("success");
+                        Log.d("jsonElement-----------",jsonElement.toString());
+                        if(jsonElement.toString().equals("true")){
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
                     }
 
                     @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
 
                     }
                 });
